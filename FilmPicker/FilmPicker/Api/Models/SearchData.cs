9929a10b04ea0b0bd45d33b6396a986c82bb226b
Rolczak <kc.rolczak@gmail.com>
2021-08-24 23:49:33 +0200

    Added config and api models

---
+++ FilmPicker/FilmPicker/Api/Models/SearchData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmPicker.Api.Models
+{
+        public class SearchData
+        {
+            public string SearchType { get; set; }
+            public string Expression { get; set; }
+
+            public List<Models.SearchData> Results { get; set; }
+
+            public string ErrorMessage { get; set; }
+        }
+}
---
+++ FilmPicker/FilmPicker/Api/Models/SearchType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmPicker.Api.Models
+{
+    public enum SearchType
+    {
+        Title = 1,
+        Movie = 2,
+        Series = 4,
+        Name = 8,
+        Episode = 16,
+        Company = 32,
+        Keyword = 64,
+        All = 128
+    }
+}
